9fd1ccc3573456900ee9dce4a0b30a060d1e47dd
Denis Gulev <denis_13@hotmail.it>
2020-02-15 17:53:00 +0100

    Change layout to Recipe view + styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -32,13 +32,13 @@ export class Recipe extends React.Component {
             {this.props.isAuthenticated ? (
               <React.Fragment>
                 <button className="button">
-                  <Link to={`/edit/${this.props.recipe.id}`}>Edit Recipe</Link>
+                  <Link to={`/edit/${this.props.recipe.id}`}>Modifica</Link>
                 </button>
                 <button
                   className="button button--secondary"
                   onClick={this.handleRemoveRequested}
                 >
-                  Remove Recipe
+                  Elimina
                 </button>
               </React.Fragment>
             ) : (
@@ -48,11 +48,20 @@ export class Recipe extends React.Component {
         </div>
         <div className="content-container recipe">
           <div className="recipe__content">
+            <div className="recipe__description">
+              <h1>{this.props.recipe.title}</h1>
+              <hr />
+              <span>PREPARAZIONE </span>
+              <span>{30 + " min"}</span>
+              <span>DIFFICOLTA' </span>
+              <span>{"media"}</span>
+              <hr />
+            </div>
             <div className="recipe__image">
-              <img src={this.props.recipe.imageUrl} />
+              <img src={this.props.recipe.imageUrl} alt="Recipe image" />
             </div>
             <div className="recipe__ingredients">
-              <span>Ingredients</span>
+              <span>Ingredienti</span>
               <ul>
                 {this.props.recipe.ingredients
                   ? this.props.recipe.ingredients.map(
@@ -65,32 +74,32 @@ export class Recipe extends React.Component {
                   : ""}
               </ul>
             </div>
-          </div>
-          <div className="recipe__description">
-            <h1>{this.props.recipe.title}</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget
-              nibh dictum, imperdiet neque vitae, lobortis enim. Nulla
-              venenatis, diam et finibus convallis, libero justo efficitur
-              nulla, vitae mollis dui lacus at ante. Phasellus sed sollicitudin
-              sem. Donec viverra lorem ac tristique elementum. Quisque dapibus
-              mollis suscipit. Mauris eleifend, quam vitae maximus tincidunt,
-              urna dui volutpat nibh, et rutrum justo turpis eu justo. Nullam
-              pellentesque sodales sem sed tincidunt. Vivamus et lobortis ipsum.
-              Nullam congue efficitur justo, et congue arcu. Etiam maximus dolor
-              ut blandit iaculis. Phasellus porttitor aliquam nibh, non viverra
-              odio auctor non. Aliquam malesuada libero sit amet felis vulputate
-              elementum. Praesent ex mauris, rutrum eu nulla a, tempus tincidunt
-              mi. Maecenas odio urna, vehicula nec venenatis a, dignissim sit
-              amet augue. Pellentesque interdum commodo libero ac sodales.
-              Curabitur consequat ex massa. Mauris cursus ante nec ex gravida
-              bibendum. Quisque lorem massa, pretium ac magna ut, ultricies
-              pharetra erat. Cras et lacus dolor. Quisque sed fermentum tortor,
-              vitae consectetur risus. Nulla sed commodo dui, eu scelerisque
-              elit. Nulla eget felis eros. Sed elit nunc, suscipit vitae tellus
-              ac, efficitur gravida risus. Sed id enim at neque aliquam
-              molestie.
-            </p>
+            <div className="recipe__preparation">
+              <span>Preparazione:</span>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                eget nibh dictum, imperdiet neque vitae, lobortis enim. Nulla
+                venenatis, diam et finibus convallis, libero justo efficitur
+                nulla, vitae mollis dui lacus at ante. Phasellus sed
+                sollicitudin sem. Donec viverra lorem ac tristique elementum.
+                Quisque dapibus mollis suscipit. Mauris eleifend, quam vitae
+                maximus tincidunt, urna dui volutpat nibh, et rutrum justo
+                turpis eu justo. Nullam pellentesque sodales sem sed tincidunt.
+                Vivamus et lobortis ipsum. Nullam congue efficitur justo, et
+                congue arcu. Etiam maximus dolor ut blandit iaculis. Phasellus
+                porttitor aliquam nibh, non viverra odio auctor non. Aliquam
+                malesuada libero sit amet felis vulputate elementum. Praesent ex
+                mauris, rutrum eu nulla a, tempus tincidunt mi. Maecenas odio
+                urna, vehicula nec venenatis a, dignissim sit amet augue.
+                Pellentesque interdum commodo libero ac sodales. Curabitur
+                consequat ex massa. Mauris cursus ante nec ex gravida bibendum.
+                Quisque lorem massa, pretium ac magna ut, ultricies pharetra
+                erat. Cras et lacus dolor. Quisque sed fermentum tortor, vitae
+                consectetur risus. Nulla sed commodo dui, eu scelerisque elit.
+                Nulla eget felis eros. Sed elit nunc, suscipit vitae tellus ac,
+                efficitur gravida risus. Sed id enim at neque aliquam molestie.
+              </p>
+            </div>
           </div>
         </div>
         <ConfirmationModal
