c299fcd281c2b6f8c6f2332ae2fb586110f01c88
denis <denis_13@hotmail.it>
2020-04-20 18:23:22 +0200

    added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => (
+  <footer className="footer">
+		<div className="footer__content">
+			<span>&copy; 2020 - Denis Gulev</span>
+    </div>
+  </footer>
+);
+
+export default Footer;
