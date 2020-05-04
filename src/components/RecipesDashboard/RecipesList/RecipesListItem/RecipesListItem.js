1095e3f193b050c3dbd634483f8ce71555d72f40
denis <denis_13@hotmail.it>
2020-05-04 09:32:50 +0200

    Added hovering animation on cards

--- src/components/RecipesDashboard/RecipesList/RecipesListItem/RecipesListItem.js
+++ src/components/RecipesDashboard/RecipesList/RecipesListItem/RecipesListItem.js
@@ -4,16 +4,15 @@ import { Link } from "react-router-dom";
 const RecipeListItem = ({ id, title, description, imageUrl }) => (
   <div className="list-item">
     <div className="list-item__image">
-      <img src={imageUrl} alt="Recipe Image" />
+			<Link to={`/recipe/${id}`}>
+				<img src={imageUrl} alt="Recipe Image" />
+      </Link>
     </div>
     <div className="list-item__content">
       <div className="list-item__heading">
         <span className="list-item__title">{title}</span>
         <span className="list-item__subtitle">{description}</span>
       </div>
-      <Link to={`/recipe/${id}`}>
-        <button className="button">VEDI</button>
-      </Link>
     </div>
   </div>
 );
