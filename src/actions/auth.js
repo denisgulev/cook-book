a6d64635625248a43fdd86767b50c78d624695b0
Denis Gulev <denis_13@hotmail.it>
2021-03-19 22:25:44 +0100

    Add persistence for user logged in the current session

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -8,25 +8,36 @@ export const login = uid => ({
 
 export const startLogin = (email, password) => {
     return () => {
-        firebase.auth()
-            .signInWithEmailAndPassword(email, password)
-            .catch(function (error) {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                // Existing and future Auth states are now persisted in the current session only.
+                firebase.auth()
+                    .signInWithEmailAndPassword(email, password)
+                    .catch(function (error) {
+                        // Handle Errors here.
+                        let errorCode = error.code;
+                        switch (errorCode) {
+                            case "auth/user-not-found":
+                                alert("Email non riconosciuta.");
+                                break;
+                            case "auth/wrong-password":
+                                alert("Password non valida.");
+                                break;
+                            case "auth/invalid-email":
+                                alert("Inserire una email valida.");
+                                break;
+                            default:
+                                break;
+                        }
+                    });
+            })
+            .catch((error) => {
                 // Handle Errors here.
-                let errorCode = error.code;
-                switch (errorCode) {
-                    case "auth/user-not-found":
-                        alert("Email non riconosciuta.");
-                        break;
-                    case "auth/wrong-password":
-                        alert("Password non valida.");
-                        break;
-                    case "auth/invalid-email":
-                        alert("Inserire una email valida.");
-                        break;
-                    default:
-                        break;
-                }
-            });
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log('errorCode - ', errorCode)
+                console.log('errorMessage - ', errorMessage)
+            })
     };
 };
 
